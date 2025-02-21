236c12fd5b3515fde96511f919199aece0e99469
Programador Full Stack 2024.1 <16133232024.1@senacrjedu.local>
2025-02-20 21:44:41 -0300

    revisao

--- src/App.java
+++ src/App.java
@@ -1,5 +1,33 @@
+import domain.model.Agencia;
+import domain.model.Pessoa;
+import domain.model.conta;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        Agencia agencia = new Agencia();
+        Pessoa pessoa = new Pessoa();
+        conta conta = new conta();
+
+
+        agencia.nome = "campo grande";
+        agencia.numero = "001";
+        agencia.email = "campogrande@bankweb.com";
+        agencia.tel = "+55 (21) 555-55555";
+
+        pessoa.cpf = "111.222.444.-99";
+        pessoa.nome = "carla dias";
+        pessoa.tel = "+55 (21) 4444-55555";
+        pessoa.email = "carladias@email.com";
+
+        conta.numero = "100001";
+        conta.agencia = agencia;
+        conta.pessoa = pessoa;
+        conta.saldo = 1000;
+        
+        System.out.println("Agencia: "+ conta.agencia.nome);
+        System.out.println("Cliente:"+ conta.pessoa.nome);
+        System.out.println("Saldo:" + conta.saldo);
     }
 }
